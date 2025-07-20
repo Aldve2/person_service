943f1f0fe87c83cecccdd284a9ad9f46721415b4
Aldve2 <jakile971012@outlook.com>
2025-07-20 12:07:00 -0500

    Update PersonController.java

--- src/main/java/yps/systems/ai/controller/PersonController.java
+++ src/main/java/yps/systems/ai/controller/PersonController.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/person")
+@RequestMapping("/personService")
 public class PersonController {
 
     private final IPersonRepository personRepository;
